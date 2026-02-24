10f0019c8788180a6491d314ee278f55a4652072
dabzflp <davidpeter685@gmail.com>
2026-02-24 17:47:30 +0000

    connection time out

--- DabzAudio_Community_Hub/server/db.js
+++ DabzAudio_Community_Hub/server/db.js
@@ -20,5 +20,8 @@ if (!process.env.DATABASE_URL) {
 
 export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false
+  ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false,
+  keepAlive: true,
+  connectionTimeoutMillis: 10000,
+  idleTimeoutMillis: 30000
 });
